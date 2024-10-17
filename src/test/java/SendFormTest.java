eb20b60b547524bf482a0c6509f2901cb929ae32
Irina <sergei32git@gmail.com>
2024-10-17 22:56:53 +0500

    fd

--- src/test/java/SendFormTest.java
+++ src/test/java/SendFormTest.java
@@ -9,11 +9,11 @@ import static com.codeborne.selenide.Selenide.open;
 public class SendFormTest {
     @Test
     void SendFormTestV1() {
-        $("[data-test-id=name] input").setValue("Сергей Новиков");
-        $("[data-test-id=phone] input").setValue("+79270000000");
-        $("[data-test-id=agreement]").click();
+        $("[data-test-id='name']").setValue("Сергей Новиков");
+        $("[data-test-id='phone']").setValue("+79270000000");
+        $("[data-test-id='agreement']").click();
         $("button").click();
-        $("[data-test-id=order-success] p")
+        $("[data-test-id='order-success']")
                 .shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
 
     }
